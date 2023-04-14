e5ba32ee8bc33f4962e31274ac4f10e8b7675cce
miguel-l-b <miguellopesbraido@gmail.com>
2023-04-14 16:21:41 -0300

    add validation

--- src/Input/MultipleChoice.java
+++ src/Input/MultipleChoice.java
@@ -1,5 +1,7 @@
 package Input;
 
 public class MultipleChoice<Type> {
+  public boolean isCorrect(Type choice) { return true; }
+  public void handleIncorrect(Type choice) {}
   public void handle(Type choice) {}
 }
